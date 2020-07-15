93c6980e17e010b8342a9d09e214bf4201a715ad
Darcy <Darcyxianau@gmail.com>
2020-07-15 19:49:13 +1000

    'init'

---
+++ src/main/java/darcy/springframework5/joke/jokewebapp/JokewebappApplication.java
@@ -0,0 +1,13 @@
+package darcy.springframework5.joke.jokewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokewebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokewebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/darcy/springframework5/joke/jokewebapp/JokewebappApplicationTests.java
@@ -0,0 +1,13 @@
+package darcy.springframework5.joke.jokewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokewebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
